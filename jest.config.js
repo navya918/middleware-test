6e3cae427e9d5fa3a9fd3de8b8633d867cc4a4f7
MTL1024 <kundanavya918@gmail.com>
2025-01-30 16:54:50 +0530

    Removed all files from Git tracking

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-// jest.config.js
-module.exports = {
-  testMatch: [
-    '**/?(*.)+(spec|test).[tj]s?(x)', // Matches test files
-  ],
-  testPathIgnorePatterns: ['/node_modules/'], // Ignores node_modules during tests
-};
